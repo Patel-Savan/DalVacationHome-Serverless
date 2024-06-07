0010162e294a7a939981175475a66c26859e0b8b
Piyush Rakeshbhai Joshi <py287300@dal.ca>
2024-06-07 19:11:08 -0300

    Initial Setup

---
+++ dalvacationhome/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./Routes/AppRoutes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ToastContainer />
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
