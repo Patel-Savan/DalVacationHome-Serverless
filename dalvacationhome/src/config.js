6fba67dba275f7cc3b07505dc25f6c9b76eadadb
himanshi <hm771904@dal.ca>
2024-07-15 23:59:09 -0300

    changes

--- dalvacationhome/src/config.js
+++ dalvacationhome/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     apiGateway: {
-      BASE_URL: "https://994990k3jb.execute-api.us-east-1.amazonaws.com/prod",
+      BASE_URL: "https://l6els62oob.execute-api.us-east-1.amazonaws.com/prod",
     },
   };
   
