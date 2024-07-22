5ab23058875c7e518968085aa57f5a35978d28d5
himanshi <hm771904@dal.ca>
2024-07-22 14:28:35 -0300

    adding user report

--- dalvacationhome/src/config.js
+++ dalvacationhome/src/config.js
@@ -3,7 +3,8 @@ const config = {
     BASE_URL: "https://ukwvch03kj.execute-api.us-east-1.amazonaws.com/prod",
   },
   lookerStudio: {
-    EMBED_URL: "https://lookerstudio.google.com/embed/reporting/a4a39011-c706-4ed9-8069-3319cc4be65c/page/qSe6D", // Replace with your Looker Studio embed URL
+    EMBED_URL1: "https://lookerstudio.google.com/embed/reporting/a4a39011-c706-4ed9-8069-3319cc4be65c/page/qSe6D", 
+    EMBED_URL2: "https://lookerstudio.google.com/embed/reporting/34446a69-5305-43bb-9dae-27b1e92bbcb9/page/Lkj6D", 
   },
 };
 
